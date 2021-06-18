f708ccf5559a8232add3d50015b429b066bf7d14
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-06-19 02:19:30 +0900

    imagetest

---
+++ server/api/image.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+template_name = [
+    "default",
+    "kinpokoblog"
+]
+
+def make_og_image(title: str, template: str) -> Image:
+    if template == "default":
+        return 
+    else:
+        template_image_path : str = os.path.join(CURRENT_DIR, f"template/{template}.png")
+        template_image = Image.open(template_image_path).copy()
+        template_image.save(f"{CURRENT_DIR}/og.png")
+
--- server/api/main.py
+++ server/api/main.py
@@ -1,15 +1,24 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+from fastapi.responses import FileResponse
+
+from .image import make_og_image, template_name
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "kinpoko"}
+@app.get("/{title}.png")
+def og_image( 
+    title: str = Path(..., max_length=50),
+    template: str = Query("default")):
 
+    title.encode('utf-8')
+    
+    if template in template_name:  
+        make_og_image(title, template)
+        return FileResponse("api/og.png")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    else:
+        return {"not":"found"}
+    
